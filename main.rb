3fcf14c77185bf2f4c4b67fc0b1a376686a10f94
jess-lynn-b <138169206+jess-lynn-b@users.noreply.github.com>
2024-01-09 04:17:03 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,9 @@
+puts "Enter the width:"
+width = gets.chomp.to_i
+puts "Enter the height:"
+height = gets.chomp.to_i
+
+area = width * height
+
+puts "Width is #{width}, Height is #{height}, Area is #{area}"
+
